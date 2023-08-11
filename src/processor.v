eadebfe32e9638d50c0653fab94a59e499b9e5b0
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2023-08-11 16:21:24 -0300

    Comentários para ID

--- src/processor.v
+++ src/processor.v
@@ -8,8 +8,8 @@ module Processor();
     wire [63:0] if_pc;
     wire [31:0] if_instr;
 
-    wire [63:0] id_pc;
-    wire [31:0] id_instr;
+    wire [63:0] id_pc; //Ligar no ID
+    wire [31:0] id_instr; //Ligar no ID
 
     Stage_if stage1_if(clk, step_clk, mem_branch_target, mem_pc_src, if_pc, if_instr);
     Bar_if_id bar1_if_id(step_clk, if_pc, id_pc, if_instr, id_instr);
