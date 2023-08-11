54ff2253bdc9d7ae66d9e816b36d22a3325ac2c6
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2023-08-11 16:19:58 -0300

    Parcial: Stage_if e Processor

---
+++ src/processor.v
@@ -0,0 +1,18 @@
+module Processor();
+
+    wire clk;
+    wire step_clk;
+
+    wire [63:0] mem_branch_target;
+    wire mem_pc_src;
+    wire [63:0] if_pc;
+    wire [31:0] if_instr;
+
+    wire [63:0] id_pc;
+    wire [31:0] id_instr;
+
+    Stage_if stage1_if(clk, step_clk, mem_branch_target, mem_pc_src, if_pc, if_instr);
+    Bar_if_id bar1_if_id(step_clk, if_pc, id_pc, if_instr, id_instr);
+
+
+endmodule
---
+++ src/stage_if.v
@@ -0,0 +1,38 @@
+`include "mux.v";
+`include "register.v";
+
+module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr);
+    input clk, step_clk, pc_src;
+    input [63:0] branch_target;
+
+    output [63:0] pc;
+    output [31:0] instr;
+
+    wire [63:0] next_pc_usual;
+    wire [63:0] next_pc;
+    wire [63:0] current_pc;
+
+    wire [63:0] static_4;    
+
+    Mux mux(pc_src, next_pc_usual, branch_target, next_pc);
+    Register pc_reg(clk, next_pc, 1, current_pc);
+    defparam pc_reg.n = 64;
+
+    Adder add(clk, current_pc, static_4, next_pc_usual);
+    defparam add.n = 64;
+
+    InstructionMemory instrMem(clk, current_pc, next_instr);
+
+    initial begin
+        static_4 = 64'b0000_0000_0000_0000_0000_0000_0000_0100;
+    end
+
+    always @(current_pc) begin
+        pc = current_pc;
+    end
+    
+    always @(next_instr) begin
+        instr = next_instr;
+    end
+
+endmodule
