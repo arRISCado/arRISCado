1a693953562d8811399e5d6715dc20ed75f4bbb5
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-08-18 15:33:04 -0300

    Memory configuration

--- src/processor.v
+++ src/processor.v
@@ -1,6 +1,15 @@
-module Processor(clk);
+module Processor(clk,
+    mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
 
     input clk;
+
+    output [31:0] mem_addr;
+    output [31:0] mem_w_data;
+    input  [31:0] mem_r_data;
+    output mem_w_enable;
+    output mem_r_enable;
+
+
     wire step_clk;
 
     wire [63:0] mem_branch_target;
@@ -38,10 +47,11 @@ module Processor(clk);
 
     Clk_generator clk_generator(clk);
     Clk_divider clk_divider(clk, step_clk);
-    defparam clk_divider.n_bit = 3;
-    defparam clk_divider.divisor = 3'd4; //???
+    clk_divider.n_bit = 3;
+    clk_divider.divisor = 4'd4; //1 clk = lê 1 byte -> min 4 clk ??
 
-    Stage_if stage1_if(clk, step_clk, mem_branch_target, mem_pc_src, if_pc, if_instr);
+    Stage_if stage1_if(clk, step_clk, mem_branch_target, mem_pc_src, if_pc, if_instr,
+        mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
     
     Bar_if_id bar1_if_id(step_clk, if_pc, id_pc, if_instr, id_instr);
 
@@ -69,7 +79,8 @@ module Processor(clk);
                 ex_wb, mem_wb,
                 ex_m, mem_m);
 
-    Stage_mem stage4_mem(clk, step_clk, mem_alu_zero, mem_alu_result, mem_read_data_2, mem_m, mem_read_data, mem_pc_src);
+    Stage_mem stage4_mem(clk, step_clk, mem_alu_zero, mem_alu_result, mem_read_data_2, mem_m, mem_read_data, mem_pc_src,
+        mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
     
     Bar_mem_wb bar4_mem_wb(step_clk, 
                 mem_read_data, wb_read_data,
