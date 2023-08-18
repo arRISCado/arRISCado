aafb450205fb64df4e27fa588e82509973be8143
llturtle22ll <angelopazin@gmail.com>
2023-08-18 15:07:20 -0300

    Corrected some sintax

--- project/cpu/decode.v
+++ project/cpu/decode.v
@@ -8,12 +8,22 @@ module decode (
     output rs1,
     output rs2,
     output shamt,
-    output func3
+    output func3,
     output func7
 );
 
 always @(posedge clock) begin
 
+
+    // Definição default de todos os sinais de controle (NOP)
+    imm = 2'b000000000000;
+    rd = 0;
+    rs1 = 0;
+    rs2 = 0;
+    shamt = 00000;    
+    func3 = 000;
+    func7 = 000;
+    
     // Eventualmente, com as instruções de 16 bits, vai ter que 
     // separar a instrução de 32 bits em 2 intruções de 16 bits 
 
@@ -25,36 +35,36 @@ always @(posedge clock) begin
         // LUI: Load Upper Immediate
         2'b0110111 : 
             begin        
-                assign rd = instruction[11:7];
-                assign imm = instruction[31:12];
+                rd = instruction[11:7];
+                imm = instruction[31:12];
             end
         
         // AUIPC: Add U-Immediate with PC
         2'b0010111 :
             begin
-                assign rd = instruction[11:7];
-                assign imm = instruction[31:12];
+                rd = instruction[11:7];
+                imm = instruction[31:12];
             end
 
         // JAL: Jump And Link
         2'b110111 : 
         begin
-            assign rd = instruction[11:7];
-            assign imm = {instruction[31], instruction[30:21], instruction[20], instruction[19:12]};
+            rd = instruction[11:7];
+            imm = {instruction[31], instruction[30:21], instruction[20], instruction[19:12]};
         end
 
         //JARL: Jump And Link Register
         2'b1100111 :
         begin
-            assign func3 = instruction[14:12];
+            func3 = instruction[14:12];
             
 
             case (func3)
             2'b000 :
                 begin
-                    assign rd = instruction[11:7];
-                    assign rs1 = instruction[19:15];
-                    assign imm = instruction[31:20];
+                    rd = instruction[11:7];
+                    rs1 = instruction[19:15];
+                    imm = instruction[31:20];
                 end
             
             endcase
@@ -69,99 +79,100 @@ always @(posedge clock) begin
             // ...
             // não pera sou eu...
             // é tenso
-            assign func3 = instruction[14:12];
+            func3 = instruction[14:12];
 
-            assign imm = {instruction[8:11], instruction[25:30], instruction[7], instruction[31]};
-            assign rs1 = instruction[25:19];
-            assign rs2 = instruction[20:24];
+            imm = {instruction[8:11], instruction[25:30], instruction[7], instruction[31]};
+            rs1 = instruction[25:19];
+            rs2 = instruction[20:24];
         end
 
         // Instruções dos tipos de Loads: dependem do func3
         2'b0000011 :
             begin
                 // Esse sinal irá indicar pra ALU/MEM qual o tipo de Load
-                assign func3 = instruction[14:12];
+                func3 = instruction[14:12];
 
-                assign rd = instruction[11:7];
-                assign rs1 = instruction[19:15];
-                assign imm = instruction[31:20];
+                rd = instruction[11:7];
+                rs1 = instruction[19:15];
+                imm = instruction[31:20];
             end
         
         // Instruções pros tipos de Save: dependem do func3
         2'b0100011 :
             begin
                 // Esse sinal irá indicar pra ALU/MEM qual o tipo de Load
-                assign func3 = instruction[14:12];
+                func3 = instruction[14:12];
  
-                assign rs1 = instruction[19:15];
-                assign rs2 = instruction[24:20];
-                assign imm = {instruction[11:7], instruction[31:25]}; 
+                rs1 = instruction[19:15];
+                rs2 = instruction[24:20];
+                imm = {instruction[11:7], instruction[31:25]}; 
             end
         
         // Instruções para operações com Imediato ou Registrador
         2'b0010011 :
             begin
-                assign func3 = instruction[14:12];
+                func3 = instruction[14:12];
 
                 // ADDI, SLTI, SLTIU, XORI, ORI, ANDI
                 if ((func3 == 2'b000) || (func3 == 2'b010) || 
                 (func3 == 2'b011) || (func3 == 2'b100) ||
                 (func3 == 2'b110) || (func3 == 2'b111)) begin
-                    assign rd = instruction[11:7];
-                    assign rs1 = instruction[19:15];
-                    assign imm = instruction[31:20];
+                    rd = instruction[11:7];
+                    rs1 = instruction[19:15];
+                    imm = instruction[31:20];
                 end
 
                 // SLLI, SRLI, SRAI
                 else if ((func3 == 001) || (func3 == 101)) begin
-                    assign func7 = instruction[31:25];
-                    assign rd = instruction[11:7];
-                    assign rs1 = instruction[19:15];
-                    assign shamt = instruction[24:20];
+                    func7 = instruction[31:25];
+                    rd = instruction[11:7];
+                    rs1 = instruction[19:15];
+                    shamt = instruction[24:20];
                 end
                 // ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
                 else begin
-                    assign func7 = instruction[31:25];
-                    assign rd = instruction[11:7];
-                    assign rs1 = instruction[19:15];
-                    assign rs2 = instruction[24:20];
+                    func7 = instruction[31:25];
+                    rd = instruction[11:7];
+                    rs1 = instruction[19:15];
+                    rs2 = instruction[24:20];
                 end
 
-        // FENCE: Synch Thread
-        2'b000111 :
-        begin
-				    assign rd = instruction[11:7];
-            assign func3 = instruction[14:12];
-            assign rs1 = instruction[19:15];
-            assign succ = instruction[22:20];
-            assign pred = instruction[26:23];
-            assign fm = instruction[27:31];        
-        end
+        // // FENCE: Synch Thread
+        // 2'b000111 :
+        // begin
+				    // rd = instruction[11:7];
+        //     func3 = instruction[14:12];
+        //     rs1 = instruction[19:15];
+        //     succ = instruction[22:20];
+        //     pred = instruction[26:23];
+        //     fm = instruction[27:31];        
+        // end
 
-        // FENCE.TSO : não faço ideia do que é isso
-        2'b0001111 :
-        begin
-            // assign rd = instruction[11:7];
-            assign func3 = instruction[14:12];
-            // assign rs1 = instruction[19:15];
-            // assign succ = instruction[22:20];
-            // assign pred = instruction[26:23];
-            // assign fm = instruction[27:31];        
-        end
+        // // FENCE.TSO : não faço ideia do que é isso
+        // 2'b0001111 :
+        // begin
+        //     // rd = instruction[11:7];
+        //     func3 = instruction[14:12];
+        //     // rs1 = instruction[19:15];
+        //     // succ = instruction[22:20];
+        //     // pred = instruction[26:23];
+        //     // fm = instruction[27:31];        
+        // end
 
         // ECALL : chamada de sistema
         2'b1110011 :
         begin
-            assign imm = instruction[31:20];
+            imm = instruction[31:20];
         end
 
         // EBREAK : chama de sistema de novo?
         2'b1110011 :
         begin
-            assign imm = instruction[31:20];
+            imm = instruction[31:20];
         end
-        default: 
-    endcase
+        default :    
+            opcode = 2'b0010011;
+        endcase
 
 end
     
