67f7d5a1c78a56c61e342b7c92fa5483be312313
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-08-18 14:48:42 -0300

    StageIF: corrected static_4 value

--- src/stage_if.v
+++ src/stage_if.v
@@ -12,7 +12,7 @@ module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr);
     wire [63:0] next_pc;
     wire [63:0] current_pc;
 
-    wire [63:0] static_4;    
+    wire [63:0] static_4 = 64'b0000_0000_0000_0000_0000_0000_0000_0100;    
 
     Mux mux(pc_src, next_pc_usual, branch_target, next_pc);
     defparam mux.n = 64;
@@ -24,10 +24,6 @@ module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr);
     defparam add.n = 64;
 
     InstructionMemory instrMem(clk, current_pc, next_instr);
-
-    initial begin
-        static_4 = 64'b0000_0000_0000_0000_0000_0000_0000_0100;
-    end
 
     always @(current_pc) begin
         pc = current_pc;
