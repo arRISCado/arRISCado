451ce76b517ce8a764081f95633a6d9875043fa7
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-08-18 15:33:04 -0300

    Memory configuration

--- src/stage_if.v
+++ src/stage_if.v
@@ -1,16 +1,25 @@
 `include "mux.v"
 `include "register.v"
 
-module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr);
+module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr,
+    mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
+    
     input clk, step_clk, pc_src;
     input [63:0] branch_target;
 
     output reg [63:0] pc;
     output reg [31:0] instr;
 
+    output [31:0] mem_addr;
+    output [31:0] mem_w_data;
+    input  [31:0] mem_r_data;
+    output mem_w_enable;
+    output mem_r_enable;
+
     wire [63:0] next_pc_usual;
     wire [63:0] next_pc;
     wire [63:0] current_pc;
+    wire [63:0] next_instr;
 
     wire [63:0] static_4 = 64'b0000_0000_0000_0000_0000_0000_0000_0100;    
 
@@ -23,7 +32,8 @@ module Stage_if(clk, step_clk, branch_target, pc_src, pc, instr);
     Adder add(current_pc, static_4, 0, next_pc_usual);
     defparam add.n = 64;
 
-    InstructionMemory instrMem(clk, current_pc, next_instr);
+    InstructionMemory instrMem(clk, current_pc, next_instr, 
+        mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
 
     always @(current_pc) begin
         pc = current_pc;
--- src/top.v
+++ src/top.v
@@ -1,6 +1,14 @@
 module top(clk);
     input clk;
 
-    Processor processor(clk);
+    wire [31:0] mem_addr;
+    wire [31:0] mem_w_data;
+    wire [31:0] mem_r_data;
+    wire mem_w_enable;
+    wire mem_r_enable;
+
+    Processor processor(clk, mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
+
+    Memory memory(mem_addr, mem_w_data, mem_r_data, mem_w_enable, mem_r_enable);
 
 endmodule
