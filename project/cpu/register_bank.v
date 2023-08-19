8a4d41cea68a29a0398683fd8afa2010b2e46da8
Iago Caran Aquino <iago.caran@gmail.com>
2023-08-19 20:29:27 -0300

    feat: add cpu top level, register_bank and alu

--- project/cpu/register_bank.v
+++ project/cpu/register_bank.v
@@ -11,30 +11,34 @@ module register_bank(
     output [31:0] value2,
 );
 
-    reg [31:0] register [31:1]
+    reg [31:0] register [31:1];
     // TODO: Add other registers (PC, SP etc)
 
+    integer i;
+    
     always @(posedge clk)
     begin
         // Write to memory
         if (write_enable) begin
             if (write_address) begin
-                register[write_address] = write value;
+                register[write_address] = write_value;
             end
         end
 
         // Update outputs
         if (rst) begin
-            register <= 32'b0;
+            for (i = 1; i < 32; i = i + 1) begin
+                register[i] = 0;
+            end
         end
         else begin
-            if (read_address1 = 0) begin
+            if (read_address1 == 32'b0) begin
                 value1 <= 32'b0;
             end
             else begin
                 value1 <= register[read_address1];
             end
-            if (read_address2 = 0) begin
+            if (read_address2 == 0) begin
                 value1 <= 32'b0;
             end
             else begin
