4f2af688d94d3c8e7fb7627b204d0ba2f1c9ffb8
EltonCN <43186596+EltonCN@users.noreply.github.com>
2023-08-20 17:21:53 -0300

    Mem clk

--- src/memory.v
+++ src/memory.v
@@ -1,4 +1,4 @@
-module Memory(addr, w_data, r_data, w_enable, r_enable);
+module Memory(clk, addr, w_data, r_data, w_enable, r_enable);
     input [31:0] addr, w_data;
     output r_data;
     input w_enable, r_enable;
