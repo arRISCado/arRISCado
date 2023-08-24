0be9e99b79fa0ff1ad56c0c9023dddaa8e8f039f
Gabriel Kinder <49625245+Kinder-Eggs@users.noreply.github.com>
2023-08-24 11:52:42 -0300

    Added tests for Branches, Compares, Loads and Stores
    Added tests for Branches, Compares, Loads and Stores with options for normal versions or No Data Hazard versions

---
+++ software/tests/TestsCompare.s
@@ -0,0 +1,23 @@
+main:
+	addi a0, zero, 3
+	addi a1, zero, -24
+	slt a2, a0, a1
+	sltu a3, a0, a1
+	slti a4, a0, 2
+	slti a5, a0, 4
+	slt a6, a1, a0
+	sltu a7, a1, a0
+	sltiu s2, a0, 1
+	sltiu s3, a0, -2
+
+# Expected final state:
+# a0: 3
+# a1: 4294967272 (-24)
+# a2: 0
+# a3: 1
+# a4: 0
+# a5: 1
+# a6: 1
+# a7: 0
+# s2: 0
+# s3: 1
