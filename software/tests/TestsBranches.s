2ce0c5dc7868686060b4106c2bd2acb53923e074
Gabriel Kinder <49625245+Kinder-Eggs@users.noreply.github.com>
2023-08-24 11:52:42 -0300

    Added tests for Branches, Compares, Loads and Stores
    Added tests for Branches, Compares, Loads and Stores with options for normal versions or No Data Hazard versions

---
+++ software/tests/TestsBranches.s
@@ -0,0 +1,44 @@
+
+main:
+	addi a1, zero, 5
+	addi a2, zero, -3
+	addi a3, zero, 5
+	addi a4, zero, 69
+	beq a1, a3, .L1
+.L0:
+	addi a0, zero, -1
+	beq zero, zero, .EXIT
+.L1:
+	beq a1, a2, .L0
+	bne a1, a3, .L0
+	bne a1, a2, .L2
+	addi a0, zero, -1
+.L2:
+	blt a1, a2, .L0
+	blt a2, a1, .L3
+	addi a0, zero, -1
+.L3:
+	bge a3, a4, .L0
+	bge a4, a3, .L4
+	addi a0, zero, -1
+.L4:
+	bge a1, a3, .L5
+	addi a0, zero, -1
+.L5:
+	bltu a2, a1, .L0
+	bltu a1, a2, .L6
+	addi a0, zero, -1
+.L6:
+	bltu a4, a3, .L0
+	bgeu a1, a2, .L0
+	bgeu a1, a3, .exit
+	addi a0, zero, -1
+.EXIT:
+	addi zero, zero, 0
+
+# Expected final state:
+# a0: 0
+# a1: 5
+# a2: 4294967293 (-3)
+# a3: 5
+# a4: 69
---
+++ software/tests/TestsLoadsStores.s
@@ -0,0 +1,18 @@
+main:
+	addi sp,sp,-8
+	addi a1, zero, 20
+	sw a1, 4(sp)
+	sh a1, 2(sp)
+	sb a1, 5(sp)
+	addi a2, a1, 93
+	sw a2, 1(sp)
+	lw a3, 4(sp)
+	lh a4, 5(sp)
+	lb a5, 2(sp)
+
+# Expected final state:
+# a1: 20
+# a2: 113
+# a3: 5120
+# a4: 20
+# a5: 0
