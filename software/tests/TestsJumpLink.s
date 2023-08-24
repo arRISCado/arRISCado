add634f002822f3b9450772886cb204be4045617
Gabriel Kinder <49625245+Kinder-Eggs@users.noreply.github.com>
2023-08-24 14:36:15 -0300

    Added test for J&L instructions
    Added test for Jump and Link instructions (JAL and JALR)

---
+++ software/tests/TestsJumpLink.s
@@ -0,0 +1,16 @@
+main:
+	jal a1, .L1
+	addi a0, zero, -1
+	j .L2
+.L1:
+	jalr a2, a1, 4
+	addi a3, zero, -2
+.L2:
+	addi zero, zero, 0
+
+
+# Expected final state:
+# a0: 0
+# a1: Memory address for the instruction after jal
+# a2: Memory address for the instruction after jalr
+# a3: 0
