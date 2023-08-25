8eda2678c50237e50d6d7fe517a7e4b002a85607
Iago <iago.caran@gmail.com>
2023-08-25 02:10:58 -0300

    feat: move testbenches to their own folder

--- testbenches/fetch_tb.v
+++ testbenches/fetch_tb.v
@@ -1,4 +1,7 @@
-module fetch_tb;
+`include "../../project/cpu/fetch.v"
+`include "../../project/rom.v"
+
+module test;
 
     // Inputs
     reg clk;
@@ -9,40 +12,62 @@ module fetch_tb;
     wire [31:0] pc;
     wire [31:0] instr;
 
+    wire [31:0] rom_data;
+    wire [31:0] rom_address;
+
     // Instantiate the fetch module
-    fetch fetch_inst (
+    fetch fetch_inst(
         .clk(clk),
         .pc_src(pc_src),
         .branch_target(branch_target),
         .pc(pc),
-        .instr(instr)
+        .instr(instr),
+        .rom_data(rom_data)
     );
 
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // Create a 10ns clock period
-    end
+    rom rom(
+        .address(pc),
+        .data(rom_data)
+    );
 
     // Testbench procedure
     initial begin
         // Initialize inputs
         pc_src = 0;
         branch_target = 32'hAABBCCDD; // Example branch target
-        
-        // Allow some time for initialization
+
         #10;
 
         // Test case 1: Sequential fetch
         $display("Test Case 1: Sequential fetch");
-        pc_src = 0;
+        $display("PC: %h, Instruction: %h", pc, instr);
+
+        clk = 0;
         #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        #10;
+
+        $display("PC: %h, Instruction: %h", pc, instr);
+
+        clk = 1;
+        #10;
+        clk = 0;
+        #10;
+
         $display("PC: %h, Instruction: %h", pc, instr);
 
         // Test case 2: Branch fetch
         $display("Test Case 2: Branch fetch");
         pc_src = 1;
+
         #10;
+        clk = 1;
+        #10;
+        clk = 0;
+        #10;
+
         $display("PC: %h, Instruction: %h", pc, instr);
 
         $finish;
