0b404392ccaf5288ce7d5be115b0af30492b8a7e
Iago <iago.caran@gmail.com>
2023-08-30 16:27:50 -0300

    feat: merge buses with pipeline steps

--- project/cpu/execute.v
+++ project/cpu/execute.v
@@ -2,76 +2,89 @@
 
 // Execute Stage
 module execute (
-    input [3:0] op, //sinal de controle sobre qual operação será feita
-    input [1:0] op_type, // sinal de controle sobre o tipo de operação(R, I, U, B)
-    input [7:0] rd,
-    input [7:0] rs1,
+    input clk,                 // Clock signal
+    input rst,                 // Reset signal
+    input [3:0] op,            // Instruction
+    input [1:0] op_type,
     input [31:0] rs1_value,
-    input [7:0] rs2,
     input [31:0] rs2_value,
     input [31:0] imm,
+    input [7:0] rd,            // Destination register
+    
     output [31:0] result
 );
 
-wire [31:0] alu_result;
-wire [3:0] alu_op; //talvez n prescise
+    wire [31:0] alu_result;
+    wire [3:0] alu_op; //talvez n prescise
 
-reg [31:0] a;
-reg [31:0] b;
+    reg [31:0] a;
+    reg [31:0] b;
 
-alu alu(op, a, b, alu_result); 
+    alu alu(op, a, b, alu_result); 
 
-always @(*)
-begin
-    case(op_type)
-    // exemplo hipotetico: op_type 000 são operações do tipo R
-    3'b000 :
+    always @(posedge clk or posedge rst)
     begin
-        // alu(op, rs1, rs2, alu_result);
-        //como que passa o RD pra escrever no banco de REGs dps?
+        if (rst)
+        begin
+            a = 0;
+            b = 0;
+        end
+        else
+        begin
+
+        end
     end
 
-    // Tipo I
-    3'b001 :
+    always @(*)
     begin
-        a = rs1_value;
-        b = imm;
-        // alu(op, mem[rs1], imm, alu_result);
-        //como que passa o RD pra escrever no banco de REGs dps?
-    end
+        case(op_type)
+        // exemplo hipotetico: op_type 000 são operações do tipo R
+        3'b000 :
+        begin
+            // alu(op, rs1, rs2, alu_result);
+            //como que passa o RD pra escrever no banco de REGs dps?
+        end
 
-    // Tipo S
-    3'b010:
-    begin
-        // alu(op, rs1, rs2, alu_result);
-        //tem imeidato nessa tbm, mas n sei onde colocar
-    end
+        // Tipo I
+        3'b001 :
+        begin
+            a = rs1_value;
+            b = imm;
+            // alu(op, mem[rs1], imm, alu_result);
+            //como que passa o RD pra escrever no banco de REGs dps?
+        end
 
-    // Tipo B
-    3'b011:
-    begin
-        // alu(op, rs1, rs2, alu_result);
-        //tem imediato, mas n sei onde colocar
-    end
+        // Tipo S
+        3'b010:
+        begin
+            // alu(op, rs1, rs2, alu_result);
+            //tem imeidato nessa tbm, mas n sei onde colocar
+        end
 
-    // Tipo U
-    3'b100:
-    begin
-        // alu(op, rd, imm, alu_result);
-        // nessa só tem rd e immediato, entao n sei como se encaixa nesse caso
-    end
+        // Tipo B
+        3'b011:
+        begin
+            // alu(op, rs1, rs2, alu_result);
+            //tem imediato, mas n sei onde colocar
+        end
 
-    // Tipo J
-    3'b101:
-    begin
-        // alu(op, rd, imm, alu_result);
-        //isso pula pra um endereço X, então acho q o mais certo é usar o PC pra fazer os cálculos
-    end
-    endcase
+        // Tipo U
+        3'b100:
+        begin
+            // alu(op, rd, imm, alu_result);
+            // nessa só tem rd e immediato, entao n sei como se encaixa nesse caso
+        end
 
-// nao acho q tudo tenha resultado a ser passado adiante
-result <= alu_result;
+        // Tipo J
+        3'b101:
+        begin
+            // alu(op, rd, imm, alu_result);
+            //isso pula pra um endereço X, então acho q o mais certo é usar o PC pra fazer os cálculos
+        end
+        endcase
 
 end
+
+    assign result = alu_result;
     
 endmodule
