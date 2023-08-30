4dc5cb58a8e81160016e95a2e9148a01c1298dc8
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2023-08-30 20:14:51 -0300

    create test for registers

---
+++ tests/register.s
@@ -0,0 +1,35 @@
+.section .text
+.globl _main
+_main:
+	addi x0, x0, 0
+	addi x1, x1, 1
+	addi x2, x2, 2
+	addi x3, x3, 3
+	addi x4, x4, 4
+	addi x5, x5, 5
+	addi x6, x6, 6
+	addi x7, x7, 7
+	addi x8, x8, 8
+	addi x9, x9, 9
+	addi x10, x10, 10
+	addi x11, x11, 11
+	addi x12, x12, 12
+	addi x13, x13, 13
+	addi x14, x14, 14
+	addi x15, x15, 15
+	addi x16, x16, 16
+	addi x17, x17, 17
+	addi x18, x18, 18
+	addi x19, x19, 19
+	addi x20, x20, 20
+	addi x21, x21, 21
+	addi x22, x22, 22
+	addi x23, x23, 23
+	addi x24, x24, 24
+	addi x25, x25, 25
+	addi x26, x26, 26
+	addi x27, x27, 27
+	addi x28, x28, 28
+	addi x29, x29, 29
+	addi x30, x30, 30
+	addi x31, x31, 31
