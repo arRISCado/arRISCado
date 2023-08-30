688b411071099df3b7ba8f739f14366da2d61c14
llturtle22ll <angelopazin@gmail.com>
2023-08-30 16:44:16 -0300

    Alu com os sinais de controle e operações

--- project/cpu/alu.v
+++ project/cpu/alu.v
@@ -1,7 +1,7 @@
 // Arithmetic Logic Unit
 
 module alu (
-  input [3:0] op,
+  input [3:0] AluControl,
   input [31:0] a,
   input [31:0] b,
   output [31:0] result,
@@ -11,22 +11,22 @@ module alu (
   wire [31:0] sub_result;
   wire [31:0] and_result;
 
-  assign add_result = A + B;
-  assign sub_result = A - B;
-  assign and_result = A & B;
+  assign add_result = a + b;
+  assign sub_result = a - b;
+  assign and_result = a & b;
 
   always @(*)
   begin
-    case (op)
-      2'b00: result = add_result; // Addition
-      2'b01: result = sub_result; // Subtraction
-      2'b10: result = and_result; // Bitwise AND
-      // Bitwise OR
-      // Bitwise XOR
-      // Bitwise NOT
-      // Shift Left
-      // Shift Right
-      // Arithmetic Shift Right
+    case (AluControl)
+      4'b0000: result = a&b; // Bitwise AND
+      4'b0001: result = a|b; // Bitwise OR
+      4'b0010: result = a+b; // Addition
+      4'b0011: result = a^b; // Bitwise XOR
+      4'b0100: result = a-b; // Subtraction
+      4'b0101: result = ~a; // Bitwise NOT
+      4'b0110: result = a<<b; // Shift Left
+      4'b0111: result = a>>b; // Shift Right
+      4'b1000: result = a>>>b; // Arithmetic Shift Right
       default: result = 32'b0; // Default output
     endcase
 
