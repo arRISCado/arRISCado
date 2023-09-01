62b56dd97b0dc032a06556b087b47766770e3f0f
Iago <iago.caran@gmail.com>
2023-09-01 17:39:15 -0300

    feat: use file to initialize rom

--- project/rom.v
+++ project/rom.v
@@ -1,10 +1,6 @@
-/* TODO: Use this to initialize the Instruction ROM
 `ifndef ROM_FILE
-`define ROM_FILE "init_rom.txt"
-`endif ROM_FILE
-
-$readmemh(`ROM_FILE, memory);
-*/
+`define ROM_FILE "../../project/init_rom.txt"
+`endif
 
 module rom (
   input [31:0] address,
@@ -14,19 +10,13 @@ module rom (
   reg [31:0] memory[255:0];
 
   initial
-  begin    
-    memory[0] = 32'h00100513; // addi a0, zero, 1
-    memory[1] = 32'h40a005b3; // sub a1, zero, a0
-    memory[2] = 32'h00a50633; // add a2, a0, a0
-    memory[3] = 32'h00c60633; // add a2, a2, a2
-    memory[4] = 32'h7ff00693; // addi a3, zero, 4095 
-    memory[5] = 32'h00b60733; // add a4, a2, a1
-    memory[6] = 32'h40e607b3; // sub a5, a2, a4
-    memory[7] = 32'h00568813; // addi a6, a3, 5
-    memory[8] = 32'h000628b7; // lui a7, 98
-    memory[9] = 32'h00000917; // auipc s2, 0
+  begin
+    // synthesis translate_off
+    $display(`ROM_FILE);
+    // synthesis translate_on
+    $readmemh(`ROM_FILE, memory, 0, 255);
   end
-
+  
   assign data = (address <= 32'd255) ? memory[address] : 32'bZ;
 
 endmodule
