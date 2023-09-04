d30278c646249666c6a3e34ec7c9cac961127e19
llturtle22ll <angelopazin@gmail.com>
2023-09-04 19:55:40 -0300

    feat: new types of instructions and constants

--- project/cpu/execute.v
+++ project/cpu/execute.v
@@ -12,7 +12,7 @@ module execute (
     // Sinais de controle
     input AluSrc,               // Determines if the value comes from the Register Bank or is an IMM
     input [2:0] AluOp,          // Operation type ALU will perform
-    input [3:0] AluControl,     // Exact operation ALU will perform
+    input [4:0] AluControl,     // Exact operation ALU will perform
     input in_MemWrite,          // True or False depending if the operation Writes in the Memory or not
     input Branch,              // True or False depending if the instruction is a Branch
     input in_MemRead,          // True or False depending if the operation Reads from the Memory or not
