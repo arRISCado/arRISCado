001e0fc2f1ea42034879489b24d0f06cfe31ce64
Joao-Pedro-MB <joaopedrobonucci@gmail.com>
2023-09-12 20:19:01 -0300

    Added PWM
    Co-authored-by: Elton Cardoso do Nascimento <EltonCN@users.noreply.github.com>

--- project/peripheral/pwm_port.v
+++ project/peripheral/pwm_port.v
@@ -4,19 +4,17 @@
 //It keeps the output on for a % of a duty cycle, and repeat it at each cycle  
 
 module pwm_port(
-    input clk; //Main clk
-    input mem_write; //If the data from memory changed
-    input [31:0] mem_data; //Data from memory
-    output reg port_out; //The port output
-)
+    input clk, //Main clk
+    input mem_write, //If the data from memory changed
+    input [31:0] mem_data, //Data from memory
+    output reg port_output //The port output
+);
     //Number of clk in one duty cycle
     //More clk in one duty cycle gives more resolution, but it worsens the perception of an analog signal
-    parameter [31:0] clk_per_cycle = 10;  
-    //Checar se parâmetro com tamanho funciona
+    parameter [31:0] clk_per_cycle = 32'd1024;  
 
-    reg [31:0] clk_on = 1'd0; //Number of cycles to keep on
-    reg [31:0] clk_off = 1'd0; //Number of cycles to keep off
-    reg [31:0] counter = 1'd0; //Number of clk that passed
+    reg [31:0] clk_on = 32'd0; //Number of cycles to keep on
+    reg [31:0] counter = 32'd0; //Number of clk that passed
 
     //PWM keeps on for clk_on cycles 
     //clk_on = max(mem_data, clk_per_cycle)
@@ -24,23 +22,30 @@ module pwm_port(
 
     //Update data from mmemory
     always @(posedge mem_write) begin
-        if(clk_per_cycle > mem_data)
-            clk_on <= clk_per_cycle;
-        else
-            clk_on <= mem_data;
+        if(mem_data > clk_per_cycle) begin
+            clk_on = clk_per_cycle;
+        end
+        else begin
+            clk_on = mem_data;
+        end
         
-        clk_off = clk_per_cycle - clk_on;
+        counter = 32'd0;
     end
 
     //Update the port output
     always @(posedge clk) begin
-        if counter > clk_per_cycle
-            counter <= 32'd0;
+        if(counter >= clk_per_cycle) begin
+            counter = 32'd0;
+        end
+        
+        if (counter >= clk_on) begin
+            port_output = 0;
+        end
+        else begin
+            port_output = 1;
+        end
 
-        if counter > clk_on
-            port_out <= 0
-        else
-            port_out <= 1
+        counter = counter + 32'd1;
     end
 
 endmodule
