92c094864091d30ed3a0ca13fdeb2b78cc918050
Kinder-Eggs <g234720@dac.unicamp.br>
2023-09-14 13:07:55 -0300

    Fix sending script for values higher than ascii limits

--- send_serial.py
+++ send_serial.py
@@ -1,21 +1,22 @@
 import serial  # pip install pyserial
 
-port = "COM4"  # Insert port connected to board here
+port = ""  # Insert port connected to board here
 
 serialPort = serial.Serial(port=port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-f = open("./tests/binaries/TestsArith.hex", "rt")
+f = open("./tests/binaries/", "rt")  # Insert .hex file to be sent
 code = f.read()
 
-ascii_string = ''
+bindata = []
 x = 0
 y = 2
 l = len(code)
 while y <= l:
-    if code[x] != '\r' and '\n':
-        ascii_string += chr(int(code[x:y], 16))
-    x += 2
-    y += 2
-print(ascii_string)
-print(str.encode(ascii_string))
-serialPort.write(str.encode(ascii_string))
+    if code[x] != '\r' and code[x] != '\n':
+        bindata.append(int(code[x:y], 16))
+        x += 2
+        y += 2
+    else:
+        x += 1
+        y += 1
+serialPort.write(bindata)
