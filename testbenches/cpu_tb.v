102c7cac3ef1ecc876d8bb145578217d6c288c9e
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-09-15 16:07:50 -0300

    Draft of pipeline test

--- testbenches/cpu_tb.v
+++ testbenches/cpu_tb.v
@@ -1,20 +1,25 @@
-`ifndef TESTBENCH
-`define TESTBENCH 1
-`endif
-
-`include "../../project/ram.v"
-`include "../../project/rom.v"
-`include "../../project/cpu/register_bank.v"
-`include "../../project/cpu/alu.v"
-`include "../../project/cpu/fetch.v"
-`include "../../project/cpu/decode.v"
-`include "../../project/cpu/execute.v"
-`include "../../project/cpu/memory.v"
-`include "../../project/cpu/writeback.v"
+`define ROM_FILE "../../testbenches/cpu_tb_rom.txt"
 `include "../../project/cpu.v"
 
 module test;
-  
-  cpu cpu();
+    reg clk;
+    reg rst;
+    
+    cpu cpu(clk, rst);
+
+    // Testbench procedure
+    initial begin
+        //Reset
+        rst = 1;
+        clk = 0;
+        #10
+        clk = 1;
+        #10
+        clk = 0;
+        rst = 0;
+
+        $display("%h", cpu.RegisterBank.register[0]);
+
+    end
 
 endmodule
