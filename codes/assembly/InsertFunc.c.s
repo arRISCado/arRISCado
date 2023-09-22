03bb88d103e41a8dd8f240c03ef773d666e7e65e
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2023-09-22 15:06:33 -0300

    Create code with vector

---
+++ codes/assembly/InsertFunc.c.s
@@ -0,0 +1,140 @@
+	.file	"InsertFunc.c"
+	.option nopic
+	.attribute arch, "rv32i2p0_m2p0_a2p0_c2p0"
+	.attribute unaligned_access, 0
+	.attribute stack_align, 16
+	.text
+	.align	1
+	.globl	troca
+	.type	troca, @function
+troca:
+	addi	sp,sp,-48
+	sw	s0,44(sp)
+	addi	s0,sp,48
+	sw	a0,-36(s0)
+	sw	a1,-40(s0)
+	lw	a5,-36(s0)
+	lw	a5,0(a5)
+	sw	a5,-20(s0)
+	lw	a5,-40(s0)
+	lw	a4,0(a5)
+	lw	a5,-36(s0)
+	sw	a4,0(a5)
+	lw	a5,-40(s0)
+	lw	a4,-20(s0)
+	sw	a4,0(a5)
+	nop
+	lw	s0,44(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	troca, .-troca
+	.align	1
+	.globl	selectionsort
+	.type	selectionsort, @function
+selectionsort:
+	addi	sp,sp,-48
+	sw	ra,44(sp)
+	sw	s0,40(sp)
+	addi	s0,sp,48
+	sw	a0,-36(s0)
+	sw	a1,-40(s0)
+	sw	zero,-20(s0)
+	j	.L3
+.L7:
+	lw	a5,-20(s0)
+	sw	a5,-28(s0)
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+	j	.L4
+.L6:
+	lw	a5,-24(s0)
+	slli	a5,a5,2
+	lw	a4,-36(s0)
+	add	a5,a4,a5
+	lw	a4,0(a5)
+	lw	a5,-28(s0)
+	slli	a5,a5,2
+	lw	a3,-36(s0)
+	add	a5,a3,a5
+	lw	a5,0(a5)
+	bge	a4,a5,.L5
+	lw	a5,-24(s0)
+	sw	a5,-28(s0)
+.L5:
+	lw	a5,-24(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+.L4:
+	lw	a4,-24(s0)
+	lw	a5,-40(s0)
+	blt	a4,a5,.L6
+	lw	a5,-20(s0)
+	slli	a5,a5,2
+	lw	a4,-36(s0)
+	add	a3,a4,a5
+	lw	a5,-28(s0)
+	slli	a5,a5,2
+	lw	a4,-36(s0)
+	add	a5,a4,a5
+	mv	a1,a5
+	mv	a0,a3
+	call	troca
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L3:
+	lw	a5,-40(s0)
+	addi	a5,a5,-1
+	lw	a4,-20(s0)
+	blt	a4,a5,.L7
+	nop
+	nop
+	lw	ra,44(sp)
+	lw	s0,40(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	selectionsort, .-selectionsort
+	.section	.rodata
+	.align	2
+.LC0:
+	.word	4
+	.word	2
+	.word	1
+	.word	3
+	.word	0
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-48
+	sw	ra,44(sp)
+	sw	s0,40(sp)
+	addi	s0,sp,48
+	li	a5,5
+	sw	a5,-20(s0)
+	lui	a5,%hi(.LC0)
+	addi	a5,a5,%lo(.LC0)
+	lw	a1,0(a5)
+	lw	a2,4(a5)
+	lw	a3,8(a5)
+	lw	a4,12(a5)
+	lw	a5,16(a5)
+	sw	a1,-40(s0)
+	sw	a2,-36(s0)
+	sw	a3,-32(s0)
+	sw	a4,-28(s0)
+	sw	a5,-24(s0)
+	addi	a5,s0,-40
+	lw	a1,-20(s0)
+	mv	a0,a5
+	call	selectionsort
+	li	a5,0
+	mv	a0,a5
+	lw	ra,44(sp)
+	lw	s0,40(sp)
+	addi	sp,sp,48
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: () 10.2.0"
