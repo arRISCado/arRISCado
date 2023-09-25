9584bbd8545008bb9c6e19915e8c734bfc706dd6
Kinder-Eggs <g234720@dac.unicamp.br>
2023-09-25 13:10:31 -0300

    Fixed all memory usage and saving with UART for instructions

--- project/uart.v
+++ project/uart.v
@@ -19,7 +19,6 @@ reg [7:0] dataIn = 0;
 reg [2:0] rxState = 0;
 reg [12:0] rxCounter = 0;
 reg [2:0] rxBitNumber = 0;
-reg byteReady = 0;
 
 localparam RX_STATE_IDLE = 0;
 localparam RX_STATE_START_BIT = 1;
@@ -34,7 +33,6 @@ always @(posedge clk) begin
                 rxState <= RX_STATE_START_BIT;
                 rxCounter <= 1;
                 rxBitNumber <= 0;
-                byteReady <= 0;
             end
         end 
         RX_STATE_START_BIT: begin
@@ -64,7 +62,32 @@ always @(posedge clk) begin
             if ((rxCounter + 1) == DELAY_FRAMES) begin
                 rxState <= RX_STATE_IDLE;
                 rxCounter <= 0;
-                byteReady <= 1;
+                case(romWriteState)
+                    RECEIVE_SIZE: begin
+                        fullsize[7:0] <= dataIn[7:0];
+                        romWriteState <= ROM_WRITE_1;
+                    end
+                    ROM_WRITE_1: begin
+                        instructionBits[7:0] <= dataIn[7:0];
+                        romWriteState <= ROM_WRITE_2;
+                    end
+                    ROM_WRITE_2: begin
+                        instructionBits[15:8] <= dataIn[7:0];
+                        romWriteState <= ROM_WRITE_3;
+                    end
+                    ROM_WRITE_3: begin
+                        instructionBits[23:16] <= dataIn[7:0];
+                        romWriteState <= ROM_WRITE_4;
+                    end
+                    ROM_WRITE_4: begin
+                        instructionBits[31:24] <= dataIn[7:0];
+                        memory[currentInst] <= instructionBits;
+                        currentInst = currentInst + 1;
+                        if(currentInst == fullsize)
+                            cpu_enable = 1;
+                        romWriteState <= ROM_WRITE_1;
+                    end
+                endcase
             end
         end
     endcase
@@ -72,7 +95,8 @@ end
 
 
 reg [31:0] memory[255:0];
-localparam currentInst = 0;
+reg [7:0] currentInst = 0;
+reg [7:0] fullsize = 0;
 
 initial begin
     repeat (256) begin
@@ -80,50 +104,16 @@ initial begin
         currentInst = currentInst + 1;
     end
     led[5:0] <= 6'b000000;
-    currentInst = 0;
+    currentInst[7:0] <= 8'b00000000;
 end
 
-reg [2:0] romWriteState = 0;
+reg [2:0] romWriteState = 4;
 reg [31:0] instructionBits = 0;
 
 localparam ROM_WRITE_1 = 0;
 localparam ROM_WRITE_2 = 1;
 localparam ROM_WRITE_3 = 2;
 localparam ROM_WRITE_4 = 3;
-integer fullbits = 32'b11111111111111111111111111111111;
-
-always @(posedge clk) begin
-    if (byteReady) begin
-        // Should be changed to use byte transmitted for something
-        // Currently changes LEDs for testing purposes
-        //led[5:2] <= ~dataIn[5:2];
-        case(romWriteState)
-            ROM_WRITE_1: begin
-                instructionBits[7:0] <= data_in[7:0];
-                romWriteState <= ROM_WRITE_2;
-            end
-            ROM_WRITE_2: begin
-                instructionBits[15:8] <= data_in[7:0];
-                romWriteState <= ROM_WRITE_3;
-            end
-            ROM_WRITE_3: begin
-                instructionBits[23:16] <= data_in[7:0];
-                romWriteState <= ROM_WRITE_4;
-            end
-            ROM_WRITE_4: begin
-                instructionBits[31:24] <= data_in[7:0];
-                led[5:0] <= instructionBits[31:26];
-                if(instructionBits[31:0] == fullbits) begin  // Not working
-                    cpu_enable = 1;
-                end
-                else begin
-                    memory[currentInst] <= instructionBits;
-                    currentInst = currentInst + 1;
-                end
-                romWriteState <= ROM_WRITE_1;
-            end
-        endcase
-    end
-end
+localparam RECEIVE_SIZE = 4;
 
 endmodule
