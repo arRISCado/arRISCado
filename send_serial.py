7e4bdd9bb309924fb176cbdc3b02e22f9645e501
Kinder-Eggs <g234720@dac.unicamp.br>
2023-09-25 11:06:17 -0300

    Modify script to send amount of bytes before sendind the code

--- send_serial.py
+++ send_serial.py
@@ -4,13 +4,17 @@ port = "COM6"  # Insert port connected to board here
 
 serialPort = serial.Serial(port=port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-f = open("./tests/binaries/test.bin", "rt")  # Insert .hex file to be sent
+f = open("./tests/binaries/TestsArith.hex", "rt")  # Insert .hex file to be sent
 code = f.read()
+code = code.replace('\n', '')
+code = code.replace('\r', '')
+code = code.replace(' ', '')
 
 bindata = []
 x = 0
 y = 2
 l = len(code)
+serialPort.write(hex(l/2))
 while y <= l:
     if code[x] != '\r' and code[x] != '\n':
         bindata.append(int(code[x:y], 16))
