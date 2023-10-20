86dc483b5e6a9e59c4a962e2081e905e73253935
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2023-10-20 16:28:00 -0300

    Assembly of cpu_tb

---
+++ tests/cpu_tb.s
@@ -0,0 +1,31 @@
+	.option nopic
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+    addi ra, zero, 5
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    sub sp, ra, ra
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi gp, sp, 3
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi tp, gp, 2
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi t0, tp, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
+    addi zero, zero, 1
