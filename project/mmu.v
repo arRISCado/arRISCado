d59fd39211f62bebd1a6c0c746d033fc4ec4a81c
Iago <iago.caran@gmail.com>
2023-11-17 17:33:27 -0300

    feat: create mmu module

--- project/mmu.v
+++ project/mmu.v
@@ -1,6 +1,54 @@
 // Memory Management Unit
 module mmu (
-    // ports
+  // Core
+  input [31:0]      c_address,
+  input [31:0]      c_data_in,
+  input             c_write_enable,
+  output reg        c_data_ready,
+  output reg [31:0] c_data_out,
+
+  // Memory
+  output reg [31:0] m_address,
+  output reg [31:0] m_data_in,
+  output reg        m_write_enable,
+  input             m_data_ready,
+  input [31:0]      m_data_out,
+
+  // Peripheral
+  output reg [31:0] p_address,
+  output reg [31:0] p_data_in,
+  output reg        p_write_enable,
+  input             p_data_ready,
+  input [31:0]      p_data_out
+
 );
-    
+  always @(*)
+  begin
+    case (c_address[31:27])
+      'b00000: // Memory
+      begin
+        p_address      <= 0;
+        p_data_in      <= 0;
+        p_write_enable <= 0;
+
+        m_address      <= c_address;
+        m_data_in      <= c_data_in;
+        m_write_enable <= c_write_enable;
+        c_data_ready   <= m_data_ready;
+        c_data_out     <= m_data_out;
+      end
+      default: // Peripheral
+      begin
+        m_address      <= 0;
+        m_data_in      <= 0;
+        m_write_enable <= 0;
+
+        p_address      <= c_address;
+        p_data_in      <= c_data_in;
+        p_write_enable <= c_write_enable;
+        c_data_ready   <= p_data_ready;
+        c_data_out     <= p_data_out;
+      end
+    endcase
+  end
 endmodule
--- project/ram.v
+++ project/ram.v
@@ -1,36 +1,28 @@
 // INSPIRED BY https://learn.lushaylabs.com/tang-nano-9k-sharing-resources/
-
 `ifndef RAM
 `define RAM
 
 module ram (
-    input clk,
-    input reset,
-    input [31:0] address,
-    input [31:0] data_in,
-    input write_enable,
-    // output [5:0] led,
-    output reg [31:0] data_out
+  input clk,
+  input reset,
+  input [31:0] address,
+  input [31:0] data_in,
+  input write_enable,
+  output reg [31:0] data_out
 );
-    reg [31:0] storage [255:0];
+  reg [31:0] storage [255:0];
 
-    // assign led = {address[5:0]};
+  integer i;
+  initial
+    for (i = 0; i <= 255; i = i + 1)
+      storage[i] <= 0;
 
-    integer i;
-    initial
-        for (i = 0; i <= 255; i = i + 1)
-            storage[i] <= 0;
+  always @(posedge clk)
+  begin
+    if (write_enable)
+      storage[address] <= data_in;
 
-    always @(posedge clk)
-    begin
-    if(address[31:29] == 3'b000) begin
-        if (write_enable)
-            storage[address] <= data_in;
-            
-        data_out <= storage[address];
-    end
-
-    end
-
+    data_out <= storage[address];
+  end
 endmodule
 `endif
