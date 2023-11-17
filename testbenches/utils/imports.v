d18db32e22bd70df2d308bb2c635a0b565dd7122
Iago <iago.caran@gmail.com>
2023-11-17 17:33:27 -0300

    feat: create mmu module

--- testbenches/utils/imports.v
+++ testbenches/utils/imports.v
@@ -12,5 +12,7 @@
 `include "../../project/cpu/memory.v"
 `include "../../project/cpu/writeback.v"
 `include "../../project/cpu.v"
+`include "../../project/mmu.v"
+`include "../../project/cache.v"
 `include "../../project/peripheral/peripheral_manager.v"
 `include "../../project/peripheral/pwm_port.v"
