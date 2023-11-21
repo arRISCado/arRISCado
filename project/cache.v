c24641b74db9b89d5e565bf640e6288dc57cff8f
Iago <iago.caran@gmail.com>
2023-11-21 10:40:08 -0300

    feat: finish cache read

--- project/cache.v
+++ project/cache.v
@@ -26,19 +26,17 @@ module cache (
   reg [31:0] data [4:0];
   reg       valid [4:0]; // TODO: Create channel to invalid positons, like when DCache changes something in ICache
 
-  wire [4:0] addr_tag = address[4:0];
-  wire [26:0] addr_index = address[31:5];
+  wire [4:0]  addr_index = address[4:0];
+  wire [26:0] addr_tag   = address[31:5];
+  
+  reg incomplete;
 
   always @(posedge clk or posedge reset)
   begin
     if (reset)
-    begin
       current_state <= IDLE;
-    end
     else
-    begin
       current_state <= next_state;
-    end
   end
 
   always @(posedge clk)
@@ -46,9 +44,9 @@ module cache (
     case (current_state)
       IDLE:
       begin
+        data_ready <= 0;
         fetch_write_enable <= 0;
         if (address != previous_address)
-          // TODO: Check for peripherals
           if (write_enable)
             next_state <= WRITE;
           else
@@ -57,35 +55,149 @@ module cache (
       end
       READ:
       begin
-        if (address[1:0] == 'b00)
+        if (~incomplete)
         begin
-          if (tag[addr_index] == addr_tag && valid[addr_index])
+          if (tag[{addr_index[4:2], 2'b00}] == addr_tag && valid[{addr_index[4:2], 2'b00}])
           begin
-            data_out <= data[addr_index];
-            data_ready <= 1;
-            next_state <= IDLE;
+            case (address[1:0])
+              2'b01:
+              begin
+                data_out <= {8'b0, data[{addr_index[4:2], 2'b00}][31:8]};
+                data_ready <= 0;
+                incomplete   <= 1;
+              end
+              2'b10:
+              begin
+                data_out <= {16'b0, data[{addr_index[4:2], 2'b00}][31:16]};
+                data_ready <= 0;
+                incomplete   <= 1;
+              end
+              2'b11:
+              begin
+                data_out <= {24'b0, data[{addr_index[4:2], 2'b00}][31:24]};
+                data_ready <= 0;
+                incomplete   <= 1;
+              end
+              default:
+              begin
+                data_out   <= data[{addr_index[4:2], 2'b00}];
+                data_ready <= 1;
+                next_state <= IDLE;
+              end
+            endcase
           end
           else
           begin
-            if (fetch_address == address)
+            if (fetch_address == {address[31:2], 2'b00})
             begin
-              data [addr_index] <= fetch_read_data;
-              tag  [addr_index] <= addr_tag;
-              valid[addr_index] <= 1;
-              data_ready        <= 1;
-              next_state  <= IDLE;
+              data [{addr_index[4:2], 2'b00}] <= fetch_read_data;
+              tag  [{addr_index[4:2], 2'b00}] <= addr_tag;
+              valid[{addr_index[4:2], 2'b00}] <= 1;
+              case (address[1:0])
+                2'b01:
+                begin
+                  data_out   <= {8'b0, fetch_read_data[31:8]};
+                  data_ready <= 0;
+                  incomplete <= 1;
+                end
+                2'b10:
+                begin
+                  data_out   <= {16'b0, fetch_read_data[31:16]};
+                  data_ready <= 0;
+                  incomplete <= 1;
+                end
+                2'b11:
+                begin
+                  data_out   <= {24'b0, fetch_read_data[31:24]};
+                  data_ready <= 0;
+                  incomplete <= 1;
+                end
+                default:
+                begin
+                  data_out   <= fetch_read_data;
+                  data_ready <= 1;
+                  next_state <= IDLE;
+                end
+              endcase
             end
             else
             begin
-              fetch_address <= address;
-              data_ready <= 0;
-              next_state <= READ;
+              fetch_address <= {address[31:2], 2'b00};
+              data_ready    <= 0;
             end
           end
         end
         else
         begin
-          // MISALIGNED
+          if (tag[{addr_index[4:2] + 1, 2'b00}] == addr_tag && valid[{addr_index[4:2] + 1, 2'b00}])
+          begin
+            case (address[1:0])
+              2'b01:
+              begin
+                data_out   <= {data[{addr_index[4:2] + 1, 2'b00}][7:0], data_out[23:0]};
+                data_ready <= 1;
+                incomplete <= 0;
+              end
+              2'b10:
+              begin
+                data_out   <= {data[{addr_index[4:2] + 1, 2'b00}][15:0], data_out[15:0]};
+                data_ready <= 1;
+                incomplete <= 0;
+              end
+              2'b11:
+              begin
+                data_out   <= {data[{addr_index[4:2] + 1, 2'b00}][23:0], data_out[7:0]};
+                data_ready <= 1;
+                incomplete <= 0;
+              end
+              default:
+              begin
+                // synthesis translate_off
+                $display("Unexpected cache error.");
+                // synthesis translate_on
+              end
+            endcase
+          end
+          else
+          begin
+            if (fetch_address == {address[31:2] + 1, 2'b00})
+            begin
+              data [addr_index] <= fetch_read_data;
+              tag  [addr_index] <= addr_tag;
+              valid[addr_index] <= 1;
+              case (address[1:0])
+                2'b01:
+                begin
+                  data_out   <= {fetch_read_data[7:0], data_out[23:0]};
+                  data_ready <= 1;
+                  incomplete <= 0;
+                end
+                2'b10:
+                begin
+                  data_out   <= {fetch_read_data[15:0], data_out[15:0]};
+                  data_ready <= 1;
+                  incomplete <= 0;
+                end
+                2'b11:
+                begin
+                  data_out   <= {fetch_read_data[23:0], data_out[7:0]};
+                  data_ready <= 1;
+                  incomplete <= 0;
+                end
+                default:
+                begin
+                  // synthesis translate_off
+                  $display("Unexpected cache error.");
+                  // synthesis translate_on
+                end
+              endcase
+            end
+            else
+            begin
+              fetch_address <= {address[31:2] + 1, 2'b00};
+              data_ready    <= 0;
+            end
+          end
         end
       end
       WRITE:
